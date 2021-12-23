6e704f93633b6f73cdae43abb72a1263657673ba
Ivan Imbert <ivan.imbert@epita.fr>
2021-12-23 19:51:37 +0100

    project component upgrade

--- mywebsite/src/components/project.js
+++ mywebsite/src/components/project.js
@@ -2,15 +2,16 @@ import React from "react";
 
 function Project({ name, image, category, description, link }) {
     return (
-        <div className="font-bold back">
-            <p>This is a project</p>
-            <div className="flex flex-line bg-gradient-to-tr font-semibold from-blue-500 to-red-500 text-gray-200 pl-16 pr-20 py-16 lg:w-screen h-1">
+        <div className="flex justify-evenly py-2 place-content-center">
+            <div className="flex flex-line bg-gradient-to-tr font-semibold from-blue-500 to-red-500 text-gray-200 px-5 py-3 w-6/7 h-min">
                 <img src={image} alt={name} width="100" height="100" className="align-left"></img>
-                <a href={link} target="_blank" rel="noreferrer" >{name}</a>
-                <p>{category}</p>
-                <p>{description}</p>
+                <div className="flex justify-evenly">
+                    <a href={link} target="_blank" rel="noreferrer" >{name}</a>
+                    <p>{category}</p>
+                    <p>{description}</p>
+                </div>
             </div>
-        </div >
+        </div>
     );
 }
 export default Project;
--- mywebsite/src/pages/projects.js
+++ mywebsite/src/pages/projects.js
@@ -5,7 +5,6 @@ import CMTAL from "../assets/CodeMeToAfterlife.png"
 function Projects() {
     return (
         <div >
-            <p>This is my Projects component !!</p>
             <Project name="CodeMeToAfterlife" image={CMTAL} category="Game, Unity" link="https://codemetoafterlife.ml/" description="A project made during my second semester of school at EPITA" />
             <Project name="CodeMeToAfterlife" image={CMTAL} category="Game, Unity" link="https://codemetoafterlife.ml/" description="A project made during my second semester of school at EPITA" />
             <Project name="CodeMeToAfterlife" image={CMTAL} category="Game, Unity" link="https://codemetoafterlife.ml/" description="A project made during my second semester of school at EPITA" />
